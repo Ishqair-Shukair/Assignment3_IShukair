1c5251e776c9b21137339dd05751e79f1185301e
ishqair shukair <ishukair@depaul.edu>
2025-05-24 00:51:45 -0500

    Add static initializer to instantiate all 3 instances

--- src/src/main/java/EagerSingleton.java
+++ src/src/main/java/EagerSingleton.java
@@ -6,4 +6,12 @@
 public class EagerSingleton {
 
     private static final EagerSingleton[] instances = new EagerSingleton[3];
+
+
+    static {
+        for (int i = 0; i < 3; i++) {
+            instances[i] = new EagerSingleton(i);
+            System.out.println("EagerSingleton " + i + " instantiated");
+        }
+    }
 }
