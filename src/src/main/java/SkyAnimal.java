0011222e74f773d14ae66203db208da90a03d6af
ishqair shukair <ishukair@depaul.edu>
2025-05-24 02:46:06 -0500

    how the sky animal flies

--- src/src/main/java/SkyAnimal.java
+++ src/src/main/java/SkyAnimal.java
@@ -7,4 +7,9 @@ public class SkyAnimal {
      * @return the name of the sky animal
      */
     String getName()
+
+    /**
+     * @return how the sky animal flies
+     */
+    String getFlying();
 }
