3740556d36b0ab4a2510360771c27cdf32dcf9f7
ishqair shukair <ishukair@depaul.edu>
2025-05-24 09:38:13 -0500

    Test lazy instances are unique

--- src/src/test/java/LazySingletonTest.java
+++ src/src/test/java/LazySingletonTest.java
@@ -2,4 +2,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LazySingletonTest {
+
+    @Test
+    public void testLazyInstancesAreUnique() {
+        LazySingleton s0 = LazySingleton.getInstance(0);
+        LazySingleton s1 = LazySingleton.getInstance(1);
+        LazySingleton s2 = LazySingleton.getInstance(2);
+
+        assertNotEquals(s0, s1);
+        assertNotEquals(s1, s2);
+        assertNotEquals(s0, s2);
+    }
 }
