0e257ccfad21775821d068d57358f1ef95cba83f
ishqair shukair <ishukair@depaul.edu>
2025-05-24 01:23:54 -0500

    Declare static array to store LazySingleton instances

---
+++ src/src/main/java/LazySingleton.java
@@ -0,0 +1,11 @@
+/**
+ * LazySingleton creates instances only when requested.
+ * Up to 3 instances total can be created.
+ */
+
+
+public class LazySingleton {
+
+    private static final LazySingleton[] instances = new LazySingleton[3];
+
+}
