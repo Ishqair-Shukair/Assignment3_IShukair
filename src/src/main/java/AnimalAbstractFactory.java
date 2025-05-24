c655ca8566503931b83e487bccc37d1abba5d787
ishqair shukair <ishukair@depaul.edu>
2025-05-24 02:33:10 -0500

    Create AnimalAbstractFactory interface with methods for animal categories

---
+++ src/src/main/java/AnimalAbstractFactory.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public interface AnimalAbstractFactory {
+    String getEra();
+    List<LandAnimal> createLandAnimals();
+    List<SeaAnimal> createSeaAnimals();
+    List<SkyAnimal> createSkyAnimals();
+}
