4df010d38ff56cd84483c2078d73426224eeb2e5
ishqair shukair <ishukair@depaul.edu>
2025-05-24 09:40:03 -0500

    Test lazy same instance returned

--- src/src/test/java/LazySingletonTest.java
+++ src/src/test/java/LazySingletonTest.java
@@ -13,4 +13,11 @@ public class LazySingletonTest {
         assertNotEquals(s1, s2);
         assertNotEquals(s0, s2);
     }
+
+    @Test
+    public void testLazyInstanceSameObject() {
+        LazySingleton a = LazySingleton.getInstance(0);
+        LazySingleton b = LazySingleton.getInstance(0);
+        assertSame(a, b);
+    }
 }
