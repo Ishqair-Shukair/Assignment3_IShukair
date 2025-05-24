d0c930c99982a2cff1097a28769032903de84e55
ishqair shukair <ishukair@depaul.edu>
2025-05-24 02:07:07 -0500

    Start client code for EagerSingleton demo

---
+++ src/src/main/java/Assignment3Singleton.java
@@ -0,0 +1,11 @@
+/**
+ * Demonstrates usage of EagerSingleton and LazySingleton.
+ */
+
+
+public class Assignment3Singleton {
+    public static void main(String[] args) {
+
+        java.util.Random r = new java.util.Random();
+    }
+}
