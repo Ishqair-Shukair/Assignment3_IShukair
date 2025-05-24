8307db40bb489386aa491fdda9cb4d3cdf7910ab
ishqair shukair <ishukair@depaul.edu>
2025-05-24 02:18:13 -0500

    Loop to retrieve EagerSingleton instances randomly

--- src/src/main/java/Assignment3Singleton.java
+++ src/src/main/java/Assignment3Singleton.java
@@ -7,5 +7,15 @@ public class Assignment3Singleton {
     public static void main(String[] args) {
 
         java.util.Random r = new java.util.Random();
+
+
+        for (int i = 0; i < 10; i++) {
+            EagerSingleton s = EagerSingleton.getInstance(r.nextInt(3));
+            System.out.println("Retrieved eager singleton " + s.getId());
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+            }
+        }
     }
 }
